3ba8ec503c816223765373c2e05624411950298e
daniyamz <terkimbiterungwa@gmail.com>
2026-06-20 15:59:37 +0100

    Punct

--- modifiers/punct.go
+++ modifiers/punct.go
@@ -2,6 +2,7 @@ package process
 
 import "strings"
 
+// helper function that validate punctuations
 func puncta(p string) bool {
 	if p == "" {
 		return false
@@ -10,6 +11,7 @@ func puncta(p string) bool {
 	return strings.ContainsRune(punc, rune(p[0]))
 }
 
+// Function that handle puntuation marks in the inpurt
 func PunctControl(str []string) string {
 	for x := 0; x < len(str); x++ {
 		if puncta(str[x]) && x > 0 {
@@ -19,10 +21,12 @@ func PunctControl(str []string) string {
 	}
 	return strings.Join(str, " ")
 }
-func QuotControl(txt string) string {
+
+// function that handle the quotetion in the input.
+func QuotControl(txt string) (string, error) {
 	text := strings.Split(txt, "'")
 	for t := 1; t < len(text); t += 2 {
 		text[t] = strings.TrimSpace(text[t])
 	}
-	return strings.Join(text, "'")
+	return strings.Join(text, "'"), nil
 }
