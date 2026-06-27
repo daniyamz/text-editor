629e99ad1c6e39233469afe26582377e53739280
daniyamz <terkimbiterungwa.com>
2026-06-27 16:08:56 +0100

    Word

--- modifiers/wordsplit.go
+++ modifiers/wordsplit.go
@@ -1,58 +1,60 @@
 package process
 
-import "strings"
+import (
+	"strings"
+)
 
 // Helper function that render punctations in an input.
-func Ispuct(s string) bool {
+func Ispuct(s rune) bool {
 
 	punt := ",./?':![]"
-	return strings.ContainsRune(punt, rune(s[0]))
+	return strings.ContainsRune(punt, s)
 }
 
 // function that convert input strings into a slice of string make each character a token.
+// It handles the punctuations from the helper function above
+// It handles bracket too making them a standalone token
 func Split(str string) ([]string, error) {
 	var token []string
-	var word string
-	count := 0
+	//var word string
+	var words strings.Builder
+
 	for _, char := range str {
-		// handling "(" as a single token.
-		if char == '(' {
-			if word != "" && count == 0 {
-				token = append(token, word)
-				word = ""
+		switch {
+		case char == '(':
+			if words.Len() != 0 {
+				token = append(token, words.String())
+				words.Reset()
 			}
-			count++
-			word += string(char)
-			// handling ")" as a single token
-		} else if char == ')' {
-			count--
-			word += string(char)
-			if count == 0 && word != "" {
-				token = append(token, word)
-				word = ""
+			token = append(token, string(char))
+			words.Reset()
+		case char == ')':
+			if words.Len() != 0 {
+				token = append(token, words.String())
+				words.Reset()
 			}
-			//handling the punctuation that as a single token.
-		} else if Ispuct(string(char)) && count == 0 {
-			if word != "" && !Ispuct(string(word[len(word)-1])) {
-				token = append(token, word)
-				word = ""
+			token = append(token, string(char))
+			words.Reset()
+		case Ispuct(char):
+			if words.Len() != 0 {
+				token = append(token, words.String())
+				words.Reset()
 			}
-			word += string(char)
-		} else if char == ' ' && count == 0 {
-			if word != "" {
-				token = append(token, word)
-				word = ""
+			token = append(token, string(char))
+		case char == ' ':
+			if words.Len() != 0 {
+				token = append(token, words.String())
+				words.Reset()
 			}
-		} else {
-			if word != "" && Ispuct(string(word[len(word)-1])) && count == 0 {
-				token = append(token, word)
-				word = ""
-			}
-			word += string(char)
+		default:
+
+			words.WriteString(string(char))
 		}
 	}
-	if word != "" {
-		token = append(token, word)
+	if words.Len() != 0 {
+		token = append(token, words.String())
+		words.Reset()
 	}
+
 	return token, nil
 }
